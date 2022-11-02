ad094174ab9acd60a9a3877ce81ddd9911d1bc31
25rae25 <whdudfo0124@gmail.com>
2022-11-02 23:00:56 +0900

    문제

--- test.js
+++ test.js
@@ -14,3 +14,32 @@ function solution(slice, n) {
 function solution(array, n) {
 	return array.filter(a => a === n).length;
   }
+
+// 점의 위치 구하기
+function solution(dot) {
+	if(dot[0] > 0 && dot[1] > 0) {
+		return 1
+	} else if(dot[0] < 0 && dot[1] > 0) {
+		return 2
+	} else if(dot[0] < 0 && dot[1] < 0) {
+		return 3
+	} else if(dot[0] > 0 && dot[1] < 0) {
+		return 4
+	}
+  }
+
+// 배열 원소의 길이
+function solution(strlist) {
+	// let result = 0
+	// let aaa = []
+	// for(let i = 0; i < strlist.length; i++) {
+	//     result = strlist[i].length
+	//     console.log(result)
+	// }
+	  return strlist.map(s => s.length);
+  }
+
+// 피자 나눠먹기 (1)
+function solution(n) {
+	return Math.floor((n - 1) / 7) + 1;
+}
