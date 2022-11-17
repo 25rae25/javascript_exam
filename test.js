a3df377d9925d6a57306ab57822e3713f6b694a6
25rae25 <whdudfo0124@gmail.com>
2022-11-17 22:31:13 +0900

    문제

--- test.js
+++ test.js
@@ -297,3 +297,13 @@ function solution(a, b) {
   
   return answer;
 }
+
+// 음양 더하기
+function solution(absolutes, signs) {
+  let answer = 0;
+  for( let i = 0; i < absolutes.length; i++ ) {
+      answer += signs[i] ? absolutes[i] : -absolutes[i] 
+  }
+  
+  return answer;
+}
