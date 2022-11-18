83dcb8cb423410b0443c258b7139db9c6ee08b8c
25rae25 <whdudfo0124@gmail.com>
2022-11-18 18:32:30 +0900

    문제

--- test.js
+++ test.js
@@ -307,3 +307,18 @@ function solution(absolutes, signs) {
   
   return answer;
 }
+
+// 폰켓몬
+function solution(nums) {
+    const answer = []
+    
+    for( let i = 0; i < nums.length; i++ ) {
+        if( 
+            (nums.length / 2) !== answer.length  &&
+            answer.includes( nums[i] ) === false
+          ) {
+            answer.push( nums[i] )
+        }
+    }
+    return answer.length
+}
