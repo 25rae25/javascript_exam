f52bc181cd1ee5b57f0305329a2a9bb2cd8035c2
25rae25 <whdudfo0124@gmail.com>
2022-11-23 15:53:13 +0900

    문제

--- test.js
+++ test.js
@@ -358,3 +358,16 @@ function solution(numbers) {
   }
   return result;
 }
+
+// 3진법 뒤집기
+function solution(n) {
+  // 3진법으로 변환
+  n = n.toString( 3 );
+  
+  let reverse = '';
+  for( let i = n.length - 1; i >= 0; i-- ) {
+      reverse += n[i]
+  }
+  // 3진법으로 변환된 데이터를 10진법으로 변환
+  return parseInt( reverse, 3 )
+}
