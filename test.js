3397c94c204144d9547d9ab81801a99203f699b3
25rae25 <whdudfo0124@gmail.com>
2022-12-06 13:51:57 +0900

    문제

--- test.js
+++ test.js
@@ -535,3 +535,12 @@ function solution(n) {
   }
   return answer[n];
 }
+
+// 문자 반복 출력하기
+function solution(my_string, n) {
+  var answer = '';
+  for(let i = 0; i< my_string.length; i++)
+      answer+=my_string[i].repeat(n);
+
+  return answer;
+}
