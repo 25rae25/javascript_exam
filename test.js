345b1def2587085d057c4e39e9b38bfb3951596a
25rae25 <whdudfo0124@gmail.com>
2022-12-12 18:33:15 +0900

    문제

--- test.js
+++ test.js
@@ -563,3 +563,8 @@ function waterMelon(n){
   }
   return result;
 }
+
+// 모음 제거
+function solution(my_string) {
+    return [...my_string].filter(str => !['a', 'e', 'i', 'o', 'u'].includes(str)).join('');
+}
