e6caa00fc62ed88e2744094a13b8fa16f24ed05b
25rae25 <whdudfo0124@gmail.com>
2022-12-14 18:30:56 +0900

    문제

--- test.js
+++ test.js
@@ -582,3 +582,12 @@ public class Solution {
         }
     }
 }
+
+// 세균 증식
+function solution(n, t) {
+  let answer = n;
+  for(let i = 0; i < t; i++){
+      answer *= 2
+  }
+  return answer;
+}
