b19576ff013d557eb9ef1da4f54d3c4882623ee2
25rae25 <whdudfo0124@gmail.com>
2023-01-08 16:26:11 +0900

    문제

--- test.js
+++ test.js
@@ -617,3 +617,27 @@ function solution(num, k) {
       return -1;
   }
 }
+
+// 가장 가까운 같은 글자
+function solution(s) {
+  let answer = [];
+  const arr = [...s]
+  for (let i = 0; i < arr.length; ++i) {
+      if(i === 0) {
+          answer[0] = -1
+      } else {
+          let count = 1;
+          for (let j = i - 1;j >= 0; --j) {
+              if (arr[i] === arr[j]) {
+                  answer[i] = count;
+                  count = 1;
+                  break;
+              } else {
+                  answer[i] = -1;
+              }
+              count++;
+          }
+      }
+  }
+  return answer;
+}
